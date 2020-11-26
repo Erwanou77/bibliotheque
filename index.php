85e4fd8b854a9ceae3436d28633b61332a662524
LAUNAY Erwan <erwan.launay77@gmail.com>
2020-11-26 22:55:02 +0100

    New file

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>The Unknown Authors Library</title>
+    <meta charset="utf-8" />
+    <?php include("styemain.php");?>
+  </head>
+  <body>
+    <?php include('header.php');?>
+    <main class="accueil">
+      <div class="index">
+        <div class="style">
+          <h1>What is "The Unknown Authors Library" ?</h1>
+          <p>
+            Sed (saepe enim redeo ad Scipionem, cuius omnis sermo erat de
+            amicitia) querebatur, quod omnibus in rebus homines diligentiores
+            essent; capras et oves quot quisque haberet, dicere posse, amicos
+            quot haberet, non posse dicere et in illis quidem parandis adhibere
+            curam, in amicis eligendis neglegentis esse nec habere quasi signa
+            quaedam et notas, quibus eos qui ad amicitias essent idonei,
+            iudicarent.Sed (saepe enim redeo ad Scipionem, cuius omnis sermo
+            erat de amicitia) querebatur, quod omnibus in rebus homines
+            diligentiores essent; capras et oves quot quisque haberet
+          </p>
+        </div>
+        <div class="img">
+          <img src="fond.jpg" width="100%" />
+        </div>
+      </div>
+    </main>
+    <?php include('footer.php');?>
+  </body>
+</html>
