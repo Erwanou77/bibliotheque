5da1640611dafea02567230a5e9fac4bed9ae535
LAUNAY Erwan <erwan.launay77@gmail.com>
2020-12-02 19:23:13 +0100

    Modif de l'index

--- index.php
+++ index.php
@@ -3,7 +3,47 @@
   <head>
     <title>The Unknown Authors Library</title>
     <meta charset="utf-8" />
-    <?php include("styemain.php");?>
+    <style type="text/css">
+    *{
+  margin : 0;
+  padding : 0;
+  }
+  .accueil{
+  padding-top: 170px;
+  background : url("img/parchemin_fond.jpg") no-repeat;
+  background-size : 100%;
+  }
+  .index{
+  display :flex;
+  align-items : center;
+  width : 100%;
+  justify-content : space-around;
+  }
+  .index .style{
+  text-align:center;
+  width : 40%;
+  margin: 0 auto;
+  }
+  .index .style h1{
+  font-size : 22px;
+  }
+.index .img{
+  display: flex;
+}
+@media screen and (max-width: 800px){
+  .index{
+    flex-direction: column-reverse;
+    background-size : 100% 100%;
+  }
+  .index .style{
+    width: 80%;
+    padding: 20px 0;
+  }
+  .index .img{
+    margin-top: 180px; 
+  }
+}
+</style>
   </head>
   <body>
     <?php include('header/header.php');?>
