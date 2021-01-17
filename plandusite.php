51a2c86c53e7cd92e57938eec8fafa1edf0a96ea
LAUNAY Erwan <erwan.launay77@gmail.com>
2021-01-17 23:14:52 +0100

    Modification style plan du site

--- plandusite.php
+++ plandusite.php
@@ -7,24 +7,24 @@
 <link rel="stylesheet" type="text/css" href="css/styleplandusite.css">
 <main class="plandusite">
 	<div class="position">
-		<h1>Plan du site : </h1>
+		<h1>Plan du site </h1>
 		<ul>
-			<li><a href="index.php">Accueil</a></li>
-			<li><a href="auteur.php">Auteurs</a>
+			<li><a href="index.php"><i class="fas fa-home"></i> Accueil</a></li><br><br>
+			<li><a href="auteur.php"><i class="fas fa-user"></i> Auteurs</a><br><br>
 				<ul>
 					<?php foreach ($pdostate as $aut) {?>
 					<li><a href="Auteur/autlist.php?id=<?php echo $aut['id'];?>"><?php echo bbcode(htmlspecialchars($aut['prenom'])) . " " . bbcode(htmlspecialchars($aut['nom']));?></a></li>
 					<?php } ?>
 				</ul>
-			</li>
-			<li><a href="bibliotheque.php">Biblioth&#232;que</a>
+			</li><br>
+			<li><a href="bibliotheque.php"><i class="fas fa-book-open"></i> Biblioth&#232;que</a><br><br>
 				<ul>
 					<?php foreach ($pdostat as $ligne) {?>
 					<li><a href="livres/details.php?isbn=<?php echo $ligne['isbn'];?>"><?php echo $ligne['titres']; ?></a></li>
 					<?php } ?>
 				</ul>
-			</li>
-			<li><a href="contact.php">Contact</a></li>
+			</li><br>
+			<li><a href="contact.php"><i class="fas fa-envelope"></i> Contact</a></li>
 		</ul>
 	</div>
 </main>
