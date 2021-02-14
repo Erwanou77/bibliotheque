43750d5256da08833c6a6682623056de8a4b2c34
matteo <m.beaulieu@ecole-ipssi.net>
2021-02-14 16:59:07 +0100

    Nouvelle BDD

--- bdd/autreq.php
+++ bdd/autreq.php
@@ -2,7 +2,7 @@
 
 require('config.php');
 if (isset($_GET['id'])) {
-		$pdostate = $bdd->query('SELECT * FROM auteurs WHERE id ='.$_GET['id']);
+		$pdostate = $bdd->query('SELECT * FROM auteur WHERE id ='.$_GET['id']);
 		$pdostate->setFetchMode(PDO::FETCH_ASSOC);
 }
 
