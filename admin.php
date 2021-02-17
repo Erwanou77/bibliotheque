926e1fc03cec6a856e4ffd95a38450245b37faca
maxime <maxime.chen@outlook.fr>
2021-02-17 09:53:30 +0100

    ajout de admin.php et listedesinscrit.php pour le back-end

---
+++ admin.php
@@ -0,0 +1,18 @@
+<?php include('header/header.php');?>
+<link rel="stylesheet" type="text/css" href="css/styleindex.css">
+<main class="accueil">
+    <div class="index">
+        <div class="style">
+<button type="button"><a href="list_inscris.php">Liste des clients inscritent</a> </button><br>
+<label for="listing">Recherche: </label>
+<input type="search" id="listing" name="liste">
+<table>
+
+</table>
+<button type="button">Modifier</button><br>
+
+
+
+
+
+<?php include('Footer/footer.php');?>
---
+++ list_inscris.php
@@ -0,0 +1,10 @@
+<?php include('header/header.php');?>
+<link rel="stylesheet" type="text/css" href="css/styleindex.css">
+<main class="accueil">
+    <div class="index">
+        <div class="style">
+          <table>
+
+          </table>
+        <button type="button">Modifier</button><br>
+        
